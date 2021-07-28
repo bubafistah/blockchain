7c8fba029dde4a70236bc7e4635de33d789dfba9
ElijaxApps <elijaxapps@gmail.com>
2021-07-28 01:51:18 +0000

    Support linux-aarch64

--- .build/lib/openssl/download.bash
+++ .build/lib/openssl/download.bash
@@ -2,18 +2,18 @@
 
 if [ ! -f "$SRC_DIR/openssl-${OPENSSL_VERSION}.tar.gz" ]; then
   cd "${SRC_DIR}" &&
-  curl -s -O "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" &&
-    echo "${OPENSSL_HASH}  openssl-${OPENSSL_VERSION}.tar.gz" | sha256sum -c
+  curl -s -O "https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_${OPENSSL_VERSION}.zip" &&
+    echo "${OPENSSL_HASH}  OpenSSL-${OPENSSL_VERSION}.tar.gz" | sha256sum -c
 fi
 
-if [ ! -d "$OPENSSL_INSTALL_DIR_AMD64" ] && [ -f "$SRC_DIR/openssl-${OPENSSL_VERSION}.tar.gz" ]; then
+if [ ! -d "$OPENSSL_INSTALL_DIR_AMD64" ] && [ -f "$SRC_DIR/Openssl-${OPENSSL_VERSION}.zip" ]; then
 
-  mkdir -p "${OPENSSL_SRC_DIR_AMD64}" &&  cd "${OPENSSL_SRC_DIR_AMD64}" && tar -xzf "${SRC_DIR}/openssl-${OPENSSL_VERSION}.tar.gz"
+  mkdir -p "${OPENSSL_SRC_DIR_AMD64}" &&  cd "${OPENSSL_SRC_DIR_AMD64}" && unzip  "${SRC_DIR}/OpenSSL-${OPENSSL_VERSION}.zip"
 
 fi
 
-if [ ! -d "$BOOST_INSTALL_DIR_ARM8" ] && [ -f "$SRC_DIR/openssl-${OPENSSL_VERSION}.tar.gz" ]; then
+if [ ! -d "$BOOST_INSTALL_DIR_ARM8" ] && [ -f "$SRC_DIR/OpenSSL-${OPENSSL_VERSION}.zip" ]; then
 
-  mkdir -p "${OPENSSL_SRC_DIR_ARM8}" && cd "${OPENSSL_SRC_DIR_ARM8}" && tar -xzf "${SRC_DIR}/openssl-${OPENSSL_VERSION}.tar.gz"
+  mkdir -p "${OPENSSL_SRC_DIR_ARM8}" && cd "${OPENSSL_SRC_DIR_ARM8}" && unzip "${SRC_DIR}/OpenSSL-${OPENSSL_VERSION}.zip"
 
 fi
